13a1c566691fda7e2ad194b1111f69909f6e2ddd
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-02-10 03:36:03 -0500

    implement validations for tour schema

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -65,7 +65,7 @@ export const createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'Failed',
-      message: 'Invalid data sent'
+      message: err
     })
   }
 }
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import slugify from 'slugify'
+import validator from 'validator'
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,7 +8,21 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
-      trim: true
+      trim: true,
+      maxlength: [
+        40,
+        'A tour name must have less or equeal than 40 characters'
+      ],
+      minlength: [
+        10,
+        'A tour name must have more or equeal than 10 characters'
+      ],
+      validate: {
+        validator: function (val) {
+          return validator.isAlpha(val.replace(/ /g, ''))
+        },
+        message: 'Tour name must only contain characters'
+      }
     },
     slug: String,
     duration: {
@@ -20,11 +35,17 @@ const tourSchema = new mongoose.Schema(
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult'
+      }
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -34,7 +55,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price
+        },
+        message: 'Dicount price ({VALUE}) must be below the regular price'
+      }
+    },
     summary: {
       type: String,
       trim: true,
