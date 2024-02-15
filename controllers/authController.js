41d29b0a97ada83a66cfe526ae35c35aee05cd90
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-02-14 19:00:35 -0500

    Implement setting new password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 import { promisify } from 'node:util'
+import crypto from 'node:crypto'
 import jwt from 'jsonwebtoken'
 import { User } from '../models/userModel.js'
 import { catchAsync } from '../utils/catchAsync.js'
@@ -153,4 +154,35 @@ export const forgotPassword = catchAsync(async (req, res, next) => {
   }
 })
 
-export const resetPassword = (req, res, next) => {}
+export const resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex')
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() }
+  })
+
+  // 2) If token has not expired and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400))
+  }
+  user.password = req.body.password
+  user.passwordConfirm = req.body.passwordConfirm
+  user.passwordResetToken = undefined
+  user.passwordResetExpires = undefined
+  await user.save()
+
+  // 3) Update changePasswordAt property for the user
+
+  // 4) Log the user in, send JWT
+  const token = signToken(user._id)
+
+  res.status(200).json({
+    status: 'success',
+    token
+  })
+})
