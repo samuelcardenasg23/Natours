8a443c6c5823ee7118da89946e1277e5cf91177f
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-02-20 17:15:24 -0500

    Build handler factory functions: Delete

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -14,16 +14,3 @@ export const deleteOne = (Model) =>
       data: null
     })
   })
-
-export const deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id)
-
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404))
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null
-  })
-})
