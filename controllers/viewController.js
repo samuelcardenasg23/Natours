944728f8c66a00da0b1d88b27b88d53d0aa1874b
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-02-26 17:24:55 -0500

    Build tour overview

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,19 @@
-const getOverview = (req, res) => {
+import { Tour } from '../models/tourModel.js'
+import { catchAsync } from '../utils/catchAsync.js'
+
+const getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from our collection
+  const tours = await Tour.find()
+
+  // 2) Build template
+
+  // 3) Render that template using tour data from step 1)
+
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All tours',
+    tours
   })
-}
+})
 
 const getTour = (req, res) => {
   res.status(200).render('tour', {
