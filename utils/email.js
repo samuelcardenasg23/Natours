a54b976f5f4b2615f42d03073e2b2b3d269206ad
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-03-12 00:27:34 -0500

    Send password reset email

--- utils/email.js
+++ utils/email.js
@@ -57,6 +57,13 @@ class Email {
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the Natours Family!')
   }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    )
+  }
 }
 
 export { Email }
